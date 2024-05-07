d6a78da6f4f731014d778bf6b128a8f75904340c
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-07 13:39:05 +0100

    Task 5 Api fetch, error handling, loading and single article component created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import ArticlesList from "./components/ArticlesList";
+import SingleArticle from "./components/SingleArticle";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<ArticlesList />}></Route>
+        <Route
+          path="/articles/:article_id"
+          element={<SingleArticle loading={loading} setLoading={setLoading} />}
+        ></Route>
       </Routes>
     </>
   );
