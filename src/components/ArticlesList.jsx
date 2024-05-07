a519ef72aea5c3dc53c7422af4e69a6c76be1174
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-07 16:32:31 +0100

    Added header component with link to home page, clickable articles

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,5 +1,6 @@
 import { getArticles } from "../api";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const ArticlesList = () => {
   const [articlesList, setArticlesList] = useState([]);
@@ -14,15 +15,17 @@ const ArticlesList = () => {
     <>
       {articlesList.map((article) => {
         return (
-          <div className="article-card" key={article.article_id}>
-            <p>
-              {article.author} posted on {article.topic}
-            </p>
-            <h2>{article.title}</h2>
-            <img src={article.article_img_url}></img>
-            <p>votes: {article.votes}</p>
-            <p>comments: {article.comment_count}</p>
-          </div>
+          <Link to={`/articles/${article.article_id}`}>
+            <div className="article-card" key={article.article_id}>
+              <p>
+                {article.author} posted on {article.topic}
+              </p>
+              <h2>{article.title}</h2>
+              <img src={article.article_img_url}></img>
+              <p>votes: {article.votes}</p>
+              <p>comments: {article.comment_count}</p>
+            </div>
+          </Link>
         );
       })}
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <Link to="/">
+        <h1>NC-NEWS</h1>
+      </Link>
+    </>
+  );
+};
+
+export default Header;
