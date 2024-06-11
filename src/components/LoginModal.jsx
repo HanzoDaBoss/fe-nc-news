225e2d1aaea4631cc5adacb8c6728427decda271
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-11 20:47:42 +0100

    Added error handling to login

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -6,8 +6,11 @@ import { UserContext } from "./contexts/User";
 import { getUsers } from "../api";
 
 const LoginModal = ({ toggleLoginModal, handleToggleLoginModal }) => {
-  const [usernameInput, setUsernameInput] = useState();
+  const [usernameInput, setUsernameInput] = useState("");
+  const [loginErrorModal, setLoginErrorModal] = useState(false);
   const { setUser } = useContext(UserContext);
+
+  const toggleLoginErrorModal = () => setLoginErrorModal(!loginErrorModal);
 
   const handleUsernameInput = (e) => {
     setUsernameInput(e.target.value);
@@ -19,35 +22,58 @@ const LoginModal = ({ toggleLoginModal, handleToggleLoginModal }) => {
       const usernameList = users.map((user) => user.username);
       if (usernameList.includes(usernameInput)) {
         setUser(usernameInput);
+        setUsernameInput("");
+        handleToggleLoginModal();
+      } else {
+        toggleLoginErrorModal();
       }
     });
   };
 
   return (
-    <Modal show={toggleLoginModal} onHide={handleToggleLoginModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>Log In</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="username"
-              autoFocus
-              onChange={handleUsernameInput}
-              value={usernameInput}
-            />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="primary" onClick={handleLoginSubmit}>
-          Login
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal show={toggleLoginModal} onHide={handleToggleLoginModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log In</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleLoginSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="username"
+                autoFocus
+                onChange={handleUsernameInput}
+                value={usernameInput}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleLoginSubmit}>
+            Login
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal
+        size="sm"
+        show={loginErrorModal}
+        onHide={toggleLoginErrorModal}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>User not found!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleLoginErrorModal}>
+            Dismiss
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
